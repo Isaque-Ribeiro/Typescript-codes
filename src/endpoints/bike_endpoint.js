9a315b9bb9a7a12d9886271a67a187b07f0aed9d
Isaque-Ribeiro <133152404+Isaque-Ribeiro@users.noreply.github.com>
2023-12-04 18:33:50 -0300

    Create bike_endpoint.js

---
+++ src/endpoints/bike_endpoint.js
@@ -0,0 +1,9 @@
+server.post("/api/bikes", async (req, res) => {
+    try {
+      const id = await app.registerBike(req.body);
+      res.status(201).json({ id });
+    } catch (e) {
+      res.status(400).json({ message: "Could not register bike." });
+      return;
+    }
+  });
